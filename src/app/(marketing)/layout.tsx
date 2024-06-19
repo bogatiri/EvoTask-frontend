baded2ea6e8396635549db0c2377ced45ddc9df6
K1ska123 <ro4evalex@gmail.com>
2024-06-19 17:03:14 +0300

    custom-scrollbar

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,7 +3,7 @@ import Header from './header'
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 
-			<main className='bg-background'>
+			<main className='bg-background custom-scrollbar'>
 				<Header />
 				{children}
 			</main>
