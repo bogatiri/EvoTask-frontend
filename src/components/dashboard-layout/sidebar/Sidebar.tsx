67c449cfbfe1132f8f3a1ad6283eb9663bd0d73a
K1ska123 <ro4evalex@gmail.com>
2024-07-26 16:39:40 +0300

    and another

--- src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -1,24 +1,28 @@
 'use client'
 
 import { useMutation } from '@tanstack/react-query'
-import { LogOut, Moon, Sun, User } from 'lucide-react'
+import { LogOut, Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
-import { SITE_NAME } from '@/constants/seo.constants'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
-import { DASHBOARD_PAGES } from '@/config/pages-url.config'
+import { SITE_NAME } from '@/constants/seo.constants'
 
 import Logo from '../../../../public/evotasklogo.svg'
 
 import { MenuItem } from './MenuItem'
 import { MENU } from './menu.data'
 import { authService } from '@/services/auth.service'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { Button } from '@/components/ui/button'
-import { useTheme } from 'next-themes'
 
 interface ISidebarProps {
 	sidebar: number | null
@@ -57,19 +61,18 @@ export function Sidebar({ sidebar }: ISidebarProps) {
 						className='w-10 h-10'
 					/>
 					{sidebar! > 9 && (
-
-					<span className='text-2xl  font-bold relative'>
-						{SITE_NAME}
-						<span className='absolute -top-1 -right-6 text-xs opacity-40 rotate-[18deg] font-normal'>
-							beta
+						<span className='text-2xl  font-bold relative'>
+							{SITE_NAME}
+							<span className='absolute -top-1 -right-6 text-xs opacity-40 rotate-[18deg] font-normal'>
+								beta
+							</span>
 						</span>
-					</span>
 					)}
 				</Link>
 				<div className='flex flex-col p-3 '>
 					{MENU.map(item => (
 						<MenuItem
-						currentUser={currentUser}
+							currentUser={currentUser}
 							sidebar={sidebar}
 							item={item}
 							key={item.link}
@@ -78,26 +81,29 @@ export function Sidebar({ sidebar }: ISidebarProps) {
 				</div>
 			</div>
 			<div>
-			<DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button
+							variant='outline'
+							size='icon'
+						>
+							<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+							<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+							<span className='sr-only'>Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='end'>
+						<DropdownMenuItem onClick={() => setTheme('light')}>
+							Light
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('dark')}>
+							Dark
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('system')}>
+							System
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 				<div className='cursor-pointer'>
 					<div
 						className={`flex gap-2.5 ${sidebar! < 10 && 'justify-center w-full'} items-center py-1.5 m-3 px-layout transition-colors hover:bg-border rounded-lg`}
--- src/components/dashboard-layout/sidebar/SmallSidebar.tsx
+++ src/components/dashboard-layout/sidebar/SmallSidebar.tsx
@@ -1,15 +1,24 @@
 'use client'
 
 import { useMutation } from '@tanstack/react-query'
-import { LogOut, PanelRightClose } from 'lucide-react'
+import { LogOut } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Separator } from '@/components/ui/separator'
 import {
 	Sheet,
-	SheetClose,
 	SheetContent,
 	SheetFooter,
 	SheetHeader,
@@ -24,9 +33,9 @@ import Logo from '../../../../public/evotasklogo.svg'
 import { MenuItem } from './MenuItem'
 import { MENU } from './menu.data'
 import { authService } from '@/services/auth.service'
-import { Separator } from '@/components/ui/separator'
 
 const SmallSidebar = () => {
+	const { setTheme } = useTheme()
 	const [currentUser, setCurrentUser] = useState('')
 	useEffect(() => {
 		const localUserId = localStorage.getItem('userId')
@@ -48,7 +57,10 @@ const SmallSidebar = () => {
 		<Sheet>
 			<SheetTrigger>
 				<div className='rounded-md m-1 bg-transition hover:text-accent-foreground'>
-					<Separator orientation='vertical' className='w-2 bg-[#858585] rounded-xl h-28' />
+					<Separator
+						orientation='vertical'
+						className='w-2 bg-[#858585] rounded-xl h-28'
+					/>
 				</div>
 			</SheetTrigger>
 			<SheetContent side='left'>
@@ -73,29 +85,50 @@ const SmallSidebar = () => {
 					</SheetTitle>
 				</SheetHeader>
 				<div className='flex flex-col h-full'>
-
 					<div className='flex flex-col h-full max-h-[85%] w-full  p-2 gap-2'>
 						{MENU.map(item => (
 							<MenuItem
-							currentUser={currentUser}
-							sidebar={20}
-							item={item}
-							key={item.link}
+								currentUser={currentUser}
+								sidebar={20}
+								item={item}
+								key={item.link}
 							/>
 						))}
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant='outline'
+									size='icon'
+								>
+									<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+									<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+									<span className='sr-only'>Toggle theme</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align='end'>
+								<DropdownMenuItem onClick={() => setTheme('light')}>
+									Light
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setTheme('dark')}>
+									Dark
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setTheme('system')}>
+									System
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
 					</div>
 					<SheetFooter className='flex flex-col sm:flex-col justify-center w-full self-end'>
-					<Separator/>
-							<div className=' w-full cursor-pointer'>
-								<div
-									className={`flex gap-2.5  items-center py-1.5 m-3 px-layout transition-colors hover:bg-border rounded-lg`}
-									onClick={() => mutate()}
-								>
-									<LogOut />
-									<span>Logout</span>
-								</div>
+						<Separator />
+						<div className=' w-full cursor-pointer'>
+							<div
+								className={`flex gap-2.5  items-center py-1.5 m-3 px-layout transition-colors hover:bg-border rounded-lg`}
+								onClick={() => mutate()}
+							>
+								<LogOut />
+								<span>Logout</span>
 							</div>
-						
+						</div>
 					</SheetFooter>
 				</div>
 			</SheetContent>
